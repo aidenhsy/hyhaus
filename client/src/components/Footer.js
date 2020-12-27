8aa6985651dfe478fb30d6af702fdc2d1d7ec8b9
Aiden Yang <aidenyang@Aidens-MacBook-Air.local>
2020-12-27 12:48:20 +0800

    bug fixes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,7 +11,6 @@ const useStyles = makeStyles((theme) => ({
   footer: {
     background: theme.palette.common.yellow,
     width: '100%',
-    height: '250px',
   },
   mainContainer: {
     position: 'absolute',
